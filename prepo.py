22fcad25d4fc356c59d618b6cd113b42120bd44a
Grass Dinosaur <artemiohuey@gmail.com>
2018-11-26 07:17:42 +1030

    Update prepo.py

--- prepo.py
+++ prepo.py
@@ -16,10 +16,19 @@ import tqdm
 # Load data
 fpaths, _, _ = load_data() # list
 
-for fpath in tqdm.tqdm(fpaths):
-    fname, mel, mag = load_spectrograms(fpath)
-    if not os.path.exists("mels"): os.mkdir("mels")
-    if not os.path.exists("mags"): os.mkdir("mags")
+# Iterator
+i = 0
 
-    np.save("mels/{}".format(fname.replace("wav", "npy")), mel)
-    np.save("mags/{}".format(fname.replace("wav", "npy")), mag)
+for fpath in tqdm.tqdm(fpaths):
+    if i < 40:
+        fname, mel, mag = load_spectrograms(fpath)
+        if not os.path.exists("mels"): os.mkdir("mels")
+        if not os.path.exists("mags"): os.mkdir("mags")
+
+        np.save("mels/{}".format(fname.replace("wav", "npy")), mel)
+        np.save("mags/{}".format(fname.replace("wav", "npy")), mag)
+    else
+        break
+
+
+
